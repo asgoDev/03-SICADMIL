2c93c637a49285c64a65649d358f5e03d32d03b2
asgoDev <asgodev@gmail.com>
2024-03-26 23:57:06 -0400

    Estilizado de menú responsive

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -2,8 +2,15 @@ import mindef from "../assets/mindef.jpg";
 import bnaval from "../assets/bnaval.jpg";
 import "../styles/app-header.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function AppHeader() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheck = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
     <header className="app-header">
       <div className="app-header__img-cont">
@@ -11,15 +18,31 @@ function AppHeader() {
         <img className="app-header__img" src={bnaval} alt="bnaval" />
       </div>
       <nav className="app-header__nav">
+        <label className="header__ico-menu-label" htmlFor="header__menu-btn">
+          <div className={`hamburger ${isChecked ? "is-active" : ""}`}>
+            <div className="_layer -top"></div>
+            <div className="_layer -mid"></div>
+            <div className="_layer -bottom"></div>
+          </div>
+        </label>
+        <input
+          className="header__menu-toggle"
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheck}
+          id="header__menu-btn"
+        />
         <ul className="app-header__ul">
           <li className="app-header__li">
-            <hr noshade='noshade' />
+            <hr noshade="noshade" />
           </li>
-          <li className="app-header__li">
+          <li className="app-header__li" onClick={handleCheck}>
             <Link to="/SICADMIL">Inicio</Link>
           </li>
-          <li className="app-header__li">
-            <Link to="/SICADMIL/inscripcion-militar-form">Planilla de Incripción Militar</Link>
+          <li className="app-header__li" onClick={handleCheck}>
+            <Link to="/SICADMIL/inscripcion-militar-form">
+              Planilla de Incripción Militar
+            </Link>
           </li>
           {/* <li className="app-header__li">
             <Link to="/SICADMIL/info">Info</Link>
