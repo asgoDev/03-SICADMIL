34dee65a15fe3b013ccbf53deed457dafd52616f
asgoDev <asgodev@gmail.com>
2024-04-23 18:31:24 -0400

    SICADMIL v1 working.

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,19 +1,30 @@
-import '../styles/select.css'
+import "../styles/select.css";
 
-function Select({id, value, setValue, options}) {
-
-  const inputControl = (e) =>{
-    let newValue = e.target.value
-    if(!newValue) return
-    setValue(newValue)
-  }
+function Select({ id, value, setValue, options }) {
+  const inputControl = (e) => {
+    let newValue = e.target.value;
+    if (!newValue) return;
+    setValue(newValue);
+  };
 
   return (
-    <select className={`complex-input__select ${!value && 'gray'}`} name={id} id={id} value={value} onChange={inputControl}>
-      <option key={0} value="">Seleccione</option>
-      {options.map((option, i) => <option key={i+1} value={option}>{option}</option>)}
+    <select
+      className={`complex-input__select ${!value && "gray"}`}
+      name={id}
+      id={id}
+      value={value}
+      onChange={inputControl}
+    >
+      <option key={0} value="">
+        Seleccione
+      </option>
+      {options.map((option, i) => (
+        <option key={i + 1} value={option}>
+          {option}
+        </option>
+      ))}
     </select>
-  )
+  );
 }
 
-export default Select
+export default Select;
